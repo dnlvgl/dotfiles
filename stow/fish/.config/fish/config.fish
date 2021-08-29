6648f7bd3057bdb1cd840a1fec01228aaab3987c
Daniel Vogel <hello@dnlvgl.com>
2021-08-29 21:30:02 +0200

    restore fish config

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,3 +1,19 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set fish_greeting
+
+# variables
+# set language to english but give german dates/measurements/currency 
+set -x LANG en_US.UTF-8
+set -x LC_ALL de_DE.UTF-8
+
+# $PATHs
+test -d ~/.local/bin; and set PATH ~/.local/bin $PATH
+test -d ~/bin; and set ~/bin $PATH
+
+# set aliases via abbreviations (expand to command)
+# remove latex temp files
+abbr texrm 'rm -f *.aux *.toc *.blg *.bbl *.log *.out *.synctex.gz'
+# show public ip
+abbr pubip 'curl ifconfig.me'
+# star trek federation ship ambient noise
+abbr warpcorebg0 'play -n synth whitenoise band -n 100 24 band -n 300 100 gain +20'
+abbr warpcorebg1 'play -n -c1 synth whitenoise lowpass -1 120 lowpass -1 120 lowpass -1 120 gain +8'
