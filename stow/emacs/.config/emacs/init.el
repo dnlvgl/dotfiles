aebb2dbfd198f06a9f7565bed303a42f33fbd5ab
Daniel Vogel <hello@dnlvgl.com>
2021-12-05 18:36:07 +0100

    indent file

--- stow/emacs/.config/emacs/init.el
+++ stow/emacs/.config/emacs/init.el
@@ -10,16 +10,16 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+			                   ("org" . "https://orgmode.org/elpa/")
+			                   ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
- (package-refresh-contents))
+  (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-   (package-install 'use-package))
+  (package-install 'use-package))
 
 (require 'use-package)
 ;; always download packages if they don't already exist
@@ -30,8 +30,8 @@
 
 ;; Jump to emacs config
 (global-set-key (kbd "C-c e") '(lambda ()
-			  (interactive)
-			  (find-file dnl/emacs-conf-org-path)))
+			                           (interactive)
+			                           (find-file dnl/emacs-conf-org-path)))
 
 ;; Reload Emacs Config
 (defun dnl/reload-init-file ()
@@ -69,8 +69,8 @@
 
 ;; but don't show line numbers in certain modes
 (dolist (mode '(org-mode-hook
-	term-mode-hook
-	eshell-mode-hook))
+	              term-mode-hook
+	              eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; transform yes-or-no questions into y-or-n
@@ -108,31 +108,31 @@
 (setq auto-save-file-name-transforms '((".*" "~/.config/emacs/auto-save-list/" t)))
 
 (use-package ivy
-:diminish
-:bind (("C-s" . swiper)
-	     :map ivy-minibuffer-map
-	     ("TAB" . ivy-alt-done)	
-	     ("C-l" . ivy-alt-done)
-	     ("C-j" . ivy-next-line)
-	     ("C-k" . ivy-previous-line)
-	     :map ivy-switch-buffer-map
-	     ("C-k" . ivy-previous-line)
-	     ("C-l" . ivy-done)
-	     ("C-d" . ivy-switch-buffer-kill)
-	     :map ivy-reverse-i-search-map
-	     ("C-k" . ivy-previous-line)
-	     ("C-d" . ivy-reverse-i-search-kill))
-:config
-(ivy-mode 1))
+  :diminish
+  :bind (("C-s" . swiper)
+	       :map ivy-minibuffer-map
+	       ("TAB" . ivy-alt-done)	
+	       ("C-l" . ivy-alt-done)
+	       ("C-j" . ivy-next-line)
+	       ("C-k" . ivy-previous-line)
+	       :map ivy-switch-buffer-map
+	       ("C-k" . ivy-previous-line)
+	       ("C-l" . ivy-done)
+	       ("C-d" . ivy-switch-buffer-kill)
+	       :map ivy-reverse-i-search-map
+	       ("C-k" . ivy-previous-line)
+	       ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-  (use-package counsel
-    :bind (
-	   ("C-x C-f" . counsel-find-file)
-	   ("C-x b" . counsel-ibuffer)
-	   ;; ("M-y" . counsel-yank-pop)
-	   ("M-x " . counsel-M-x))
-    :config
-    (counsel-mode 1))
+(use-package counsel
+  :bind (
+	       ("C-x C-f" . counsel-find-file)
+	       ("C-x b" . counsel-ibuffer)
+	       ;; ("M-y" . counsel-yank-pop)
+	       ("M-x " . counsel-M-x))
+  :config
+  (counsel-mode 1))
 
 (setq-default tab-width 2)
 (setq-default evil-shift-width tab-width)
@@ -194,18 +194,18 @@
 
   ;; theme source blocks like in native mode
   (setq org-src-fontify-natively t
-      org-src-tab-acts-natively t
-      org-confirm-babel-evaluate nil
-      org-edit-src-content-indentation 0)
+        org-src-tab-acts-natively t
+        org-confirm-babel-evaluate nil
+        org-edit-src-content-indentation 0)
 
   (dnl/org-font-setup))
 
 ;; use custom bullets instead of stars for headings
 (use-package org-bullets
-:after org
-:hook (org-mode . org-bullets-mode)
-:custom
-(org-bullets-bullet-list '("●" "○" "◉" "○" "●" "○" "●")))
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("●" "○" "◉" "○" "●" "○" "●")))
 
 
 ;; use visual-fill-column to center org mode content
@@ -235,16 +235,3 @@
   (global-diff-hl-mode)
   (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key visual-fill-column use-package spacemacs-theme org-bullets magit evil-nerd-commenter emojify doom-modeline diff-hl counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
