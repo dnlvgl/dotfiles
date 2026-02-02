50d35b720ed1199e61d3510550da4eb7804685cf
Daniel Vogel <hello@dnlvgl.com>
2026-02-02 10:34:48 +0100

    move emacs away from lirate config, just use init.el

--- stow/emacs/.config/emacs/init.el
+++ stow/emacs/.config/emacs/init.el
@@ -1,4 +1,15 @@
-;; use dnl prefixed file, 
+;;; init.el --- Emacs Configuration -*- lexical-binding: t -*-
+
+;; This file uses outline-minor-mode for section folding.
+;; Use M-g o (consult-outline) to jump between sections.
+
+;;; Custom Variables
+
+;; Setup misc variables to use later.
+;; 'Per System' variables are loaded via an dnl-custom-vars.el, if this does not
+;; exist use fallback values
+
+;; use dnl prefixed file,
 (setq custom-file "~/.config/emacs/dnl-custom-vars.el")
 
 (cond ((file-exists-p custom-file)
@@ -10,16 +21,17 @@
         (defvar dnl/default-font-size 130)
         (defvar dnl/org-agenda-path "~/Sync/org/")
         ))
- 
 
- (defvar dnl/emacs-conf-path "~/.config/emacs/")
+ ;; Kept for backward compatibility, no longer actively used
  (defvar dnl/emacs-conf-org-path "~/.config/emacs/emacs.org")
- (defvar dnl/emacs-conf-init-path "~/.config/.emacs/init.el")
+ (defvar dnl/emacs-conf-init-path "~/.config/emacs/init.el")
 ;; Try Fira for Larger Screen slowness
 ;; else try: https://www.reddit.com/r/emacs/comments/zgp6kw/gui_emacs_weird_the_bigger_the_frame_size_is_the/
 ;; (defvar dnl/default-font "Iosevka SS08")
  (defvar dnl/default-font "Fira Code") ;; sudo dnf install fira-code-fonts
  (defvar dnl/indent-width 2)
+
+;;; Package Management
 
 (require 'package)
 
@@ -39,26 +51,17 @@
 ;; always download packages if they don't already exist
 (setq use-package-always-ensure t)
 
+;;; Keyboard Bindings
+
+;; General rebindings
+
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Jump to emacs config
 (global-set-key (kbd "C-c e") '(lambda ()
 			                           (interactive)
-			                           (find-file dnl/emacs-conf-org-path)))
-
-;; Reload Emacs Config
-(defun dnl/reload-init-file ()
-  (interactive)
-  ;; export config to init.el
-  (org-babel-tangle-file dnl/emacs-conf-org-path dnl/emacs-conf-init-path)
-  ;; reload init file
-  (load-file user-init-file)
-  ;; restart org mode, else there are some hook issues. Disabled as also annoying if not changing stuff for org, run manually if needed
-  ;;(org-mode-restart)
-  )
-
-(global-set-key (kbd "C-c r") 'dnl/reload-init-file) 
+			                           (find-file user-init-file)))
 
 ;; Show buffer menu
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
@@ -73,14 +76,30 @@
 ;; jump to file under cursor, problems without file ending, use xref-find-definition or projectile?
 (global-set-key (kbd "C-c f") 'find-file-at-point)
 
+;;;; god mode
+
+;; <menu> is bound to caps lock in os settings. <menu> normally opens the m-x
+;; buffer, would be sweet to have as an actual key. what to replace the god-mode
+;; trigger with? <escape> already is used as an alternative for C-g to quit
+;; menus.
+
 (use-package god-mode
   :bind (("<menu>" . god-local-mode)))
+
+;;;; which-key
+
+;; Show possible keybindings after short delay
 
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1))
+
+;;; General Config
+
+;; At work emacs does not see all shell path by default under macos. This fixes
+;; stuff like lsp not being found
 
 (use-package exec-path-from-shell
   :ensure t
@@ -89,6 +108,8 @@
     (setq exec-path-from-shell-shell-name "fish")
     (setq exec-path-from-shell-arguments '("-l" "-i"))
     (exec-path-from-shell-initialize)))
+
+;;;; UI
 
 (setq inhibit-startup-message t)
 
@@ -126,16 +147,24 @@
 ;; Pick up the changes from disk automatically
 (global-auto-revert-mode 1)
 
+;;;; Theme
+
+;; Load custom theme from the themes folder
+
 (add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
 (load-theme 'calcite-light t)
 
 
-(defun dnl/reload-theme ()                                                                                                              
-  "Reload the current theme without confirmation."                                                                                  
-  (interactive)                                                                                                                     
-  (let ((current-themes custom-enabled-themes))                                                                                     
-    (mapc #'disable-theme custom-enabled-themes)                                                                                    
+(defun dnl/reload-theme ()
+  "Reload the current theme without confirmation."
+  (interactive)
+  (let ((current-themes custom-enabled-themes))
+    (mapc #'disable-theme custom-enabled-themes)
     (mapc (lambda (theme) (load-theme theme t)) current-themes)))
+
+;; Switch theme according to system theme. Linux: current used Emacs 30.2 uses
+;; GTK3 Theme and needs e.g. Legacy GTK3 Switcher Extension +
+;; ~dnf install adw-gtk3-theme~ and set it in gnome tweaks
 
 (use-package auto-dark
   :custom
@@ -146,6 +175,8 @@
   (auto-dark-allow-powershell nil)
   :init (auto-dark-mode))
 
+;; Icons similiar to all-the-icons, e.g used by doom-modeline
+
 (use-package nerd-icons
   ;; :custom
   ;; The Nerd Font you want to use in GUI
@@ -154,14 +185,28 @@
   ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
   )
 
+;;;; Font
+
 (set-face-attribute 'default nil :font dnl/default-font :height dnl/default-font-size)
 
 ;; Set the fixed pitch face
 (set-face-attribute 'fixed-pitch nil :font dnl/default-font :height  dnl/default-font-size)
 
+;; Use emojis everywhere
+
+;;(use-package emojify
+;;  :hook (after-init . global-emojify-mode))
+
+;;;; Modeline
+
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
+
+;;;; Saving and Backup
+
+;; All backup files get sent to ~/.config/emacs/backups/ to not clutter the
+;; file system
 
 (setq backup-directory-alist '(("." . "~/.config/emacs/backups/")))
 (setq delete-old-versions -1)
@@ -169,12 +214,19 @@
 (setq vc-make-backup-files t)
 (setq auto-save-file-name-transforms '((".*" "~/.config/emacs/auto-save-list/" t)))
 
+;;;; Completion
+
+;; Use Ivy + Counsel for more convenient completion systems. Basically replace
+;; emacs own completion
+;;
+;; Currently disabled to test out vertico, consult, embark, orderless, see below
+
 (use-package ivy
   :disabled
   :diminish
   :bind (("C-s" . swiper)
 	       :map ivy-minibuffer-map
-	       ("TAB" . ivy-alt-done)	
+	       ("TAB" . ivy-alt-done)
 	       ("C-l" . ivy-alt-done)
 	       ("C-j" . ivy-next-line)
 	       ("C-k" . ivy-previous-line)
@@ -199,6 +251,7 @@
   (counsel-mode 1))
 
 ;; Completion systems based on the standard Emacs completing-read API
+
 (use-package consult
   ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings (mode-specific-map)
@@ -258,8 +311,10 @@
   :hook (completion-list-mode . consult-preview-at-point-mode))
 
 
-;; completion style that divides the pattern into space-separated components, and matches candidates that match all of the components in any order
+;; completion style that divides the pattern into space-separated components,
+;; and matches candidates that match all of the components in any order
 ;; e.g.: searching "region indent" will match with "indent-region"
+
 (use-package orderless
   :init
   (setq completion-styles '(orderless)
@@ -267,6 +322,7 @@
         completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Vertical completion UI based on the default completion system
+
 (use-package vertico
   :init
   (vertico-mode)
@@ -285,11 +341,13 @@
   )
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
+
 (use-package savehist
   :init
   (savehist-mode))
 
 ;; Enable richer annotations using the Marginalia package
+
 (use-package marginalia
   ;; Either bind `marginalia-cycle` globally or only in the minibuffer
   :bind (("M-A" . marginalia-cycle)
@@ -302,7 +360,10 @@
   ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode))
 
-;; Sort of right-click contextual menu for Emacs, accessed through the embark-act command (which you should bind to a convenient key), offering you relevant actions to use on a target determined by the context
+;; Sort of right-click contextual menu for Emacs, accessed through the
+;; embark-act command (which you should bind to a convenient key), offering you
+;; relevant actions to use on a target determined by the context
+
 (use-package embark
   :bind
   (("C-." . embark-act)         ;; C-. by default but does not work on gnome, run ~gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"~ to get it back
@@ -321,6 +382,7 @@
                  (window-parameters (mode-line-format . none)))))
 
 ;; Consult users will also want the embark-consult package.
+
 (use-package embark-consult
   :after (embark consult)
   :demand t ; only necessary if you have the hook below
@@ -328,6 +390,8 @@
   ;; auto-updating embark collect buffer
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
+
+;; use corfu for completion at point
 
 ;;(use-package corfu
   ;;:custom
@@ -346,7 +410,13 @@
   (setq corfu-popupinfo-delay '(1.25 . 0.5))
   (corfu-popupinfo-mode 1)) ; shows documentation after `corfu-popupinfo-delay'
 
+;;;; Movement
+
+;; Enable windmove to use ~SHIFT+arrow~ to move between windows
+
 (windmove-default-keybindings)
+
+;; Switches cursor automatically to new splitted window
 
 (defun split-and-follow-horizontally ()
     (interactive)
@@ -362,9 +432,14 @@
     (other-window 1))
 (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
+;; Move around with avy. Lots of feature, lets start simple with jumping to
+;; chars. Would be cool to have a
+
 (use-package avy
       :bind
     (("C-z" . avy-goto-char)))
+
+;;;; Dashboard
 
 (use-package dashboard
   ;; only show dasboard if opening emacs without file
@@ -374,14 +449,19 @@
   (setq dashboard-banner-logo-title "YOUR ADD HERE")
   (setq dashboard-set-footer nil)
   (setq dashboard-startup-banner "~/.config/emacs/dashboard-logos/orb.png")
-  ;; (setq dashboard-startup-banner "~/.config/emacs/dashboard-logos/rms-pepper.png")  
+  ;; (setq dashboard-startup-banner "~/.config/emacs/dashboard-logos/rms-pepper.png")
   (setq dashboard-items '((bookmarks . 10)
-                          (agenda . 5)                            
+                          (agenda . 5)
                           ;;(projects . 5)
                           (recents . 0)
                           (registers . 5)))
   (setq dashboard-center-content t)
   (dashboard-setup-startup-hook))
+
+;;;; Terminal
+
+;; Use Vterm, faster than term. Needed to install ~sudo dnf install cmake
+;; libtool~. Needs some more shell-side configuration
 
 (use-package vterm
   ;;:when (bound-and-true-p module-file-suffix)
@@ -397,12 +477,44 @@
   (setq vterm-kill-buffer-on-exit t
         vterm-shell "/usr/bin/fish"))
 
+;;;; File Management
+
+;; Using dired for file management, some basic tweaks
+
 (use-package dired
   :ensure nil
   :config (setq dired-listing-switches "-agho --group-directories-first"))
 
 (when (eq system-type 'darwin)
   (setq insert-directory-program "/opt/homebrew/bin/gls"))
+
+;;;; LLMs
+
+;; Be wrong faster
+;;
+;; NOTE: This configuration is currently disabled but preserved for reference.
+;; Uncomment and configure with your actual Azure proxy URL to enable.
+
+;; (use-package gptel
+;;   :ensure nil
+;;   :config
+;;   ;; Remove default backend
+;;   (setq gptel--known-backends nil))
+;;
+;; ;; Internal OpenAI proxy, only reachable from VPN
+;; (gptel-make-azure "Azure-1"             ; Name, whatever you'd like
+;;   :protocol "https"                     ; Optional -- https is the default
+;;   :host dnl/llm-azure-proxy-url         ; Don't want to leak the internal url
+;;   :endpoint "/v1/chat/completions"      ; Specify endpoint if needed
+;;   :stream t                             ; Enable streaming responses
+;;   :key "foo"                            ; Replace with your Azure API key
+;;   :models '("o1" "o3"))                 ; Specify the model(s) you want to use
+
+;;; File Editing
+
+;;;; Indentation
+
+;; Use 2 spaces by default
 
 (setq-default tab-width dnl/indent-width)
 (setq-default indent-tabs-mode nil)
@@ -412,8 +524,14 @@
   :config
   (editorconfig-mode 1))
 
+;;;; Commenting Lines
+
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+;;;; Brackets
+
+;; Highlight and autoclose brackets
 
 ;; Autoclose brackets
 (electric-pair-mode 1)
@@ -422,10 +540,14 @@
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 
+;;;; Move current line or region up and down
+
 (use-package move-text
-;; meta shift to not collide with orgmode                 
+;; meta shift to not collide with orgmode
   :bind(("M-S-<up>" . move-text-up)
         ("M-S-<down>" . move-text-down)))
+
+;;; Org Mode
 
 (use-package org
   :bind(("C-c a" . org-agenda)
@@ -473,6 +595,12 @@
   :custom
   (org-modern-star nil))
 
+;;;; Block Templates
+
+;; These templates enable you to type things like =<el= and then hit =Tab= to
+;; expand the template. More documentation can be found at the Org Mode Easy
+;; Templates documentation page.
+
 ;; This is needed as of Org 9.2
 (require 'org-tempo)
 
@@ -483,12 +611,19 @@
 (add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
 (add-to-list 'org-structure-template-alist '("json" . "src json"))
 
+;;;; Capture Web Bookmarks to orgmode
+
 (use-package org-web-tools)
 
 (defun dnl/org-web-tools-insert-link-for-clipboard-url ()
   "Extend =org-web-tools-inster-link-for-url= to take URL from clipboard or kill-ring"
   (interactive)
   (org-web-tools--org-link-for-url (org-web-tools--get-first-url)))
+
+;;; Git
+
+;; Does it make sense to auto-refresh Magit status buffers?
+;; `(setq magit-auto-revert-mode t)`
 
 (use-package magit)
 
@@ -498,6 +633,14 @@
   (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
   (add-hook 'after-revert-hook 'diff-hl-update))
+
+;;; Language Support
+
+;;;; Web
+
+;;;;; HTML
+
+;; Minimal setup for HTML and mixed modes
 
 (use-package web-mode
   :config
@@ -516,6 +659,15 @@
            ;;(setq web-mode-enable-current-element-highlight t)
            )
 
+;;;;;; TODO move tsx out of here and find a better way to integrate it with ts-mode and eglot
+
+;; Check this post: https://vxlabs.com/2022/06/12/typescript-development-with-emacs-tree-sitter-and-lsp-in-2022/?utm_source=pocket_reader
+
+;;;;; Emmet
+
+;; Use emmet to complete HTML abbreviations via ~C-j~. So ~div~ becomes
+;; ~<div></div>~
+
 (use-package emmet-mode
   :hook ((web-mode . emmet-mode))
   :init
@@ -527,6 +679,10 @@
                (if (string= web-mode-cur-language "css")
            (setq emmet-use-css-transform t)
            (setq emmet-use-css-transform nil))))))
+
+;;;;; JS/TS
+
+;; Syntax Highlighting for Javascript + Typescript
 
 (use-package typescript-ts-mode
   :after eglot
@@ -542,13 +698,34 @@
   :config
   (setq js-indent-level dnl/indent-width))
 
+;;;;;; TODO add yasnippet with complete function definitions
+
+;;;;; CSS
+
+(use-package css-ts-mode
+  :after eglot
+  :mode "\\.css\\'"
+  :config
+  (setq css-ts-mode-indent-offset dnl/indent-width))
+
+;;;;; JSON
+
+(use-package json-ts-mode
+  :after eglot
+  :mode "\\.json\\'"
+  :config
+  (setq json-ts-mode-indent-offset dnl/indent-width))
+
+;;;; Lisp Stuff
+
 ;;(use-package lispy
 ;;  :hook ((emacs-lisp-mode . lispy-mode)
 ;;         (scheme-mode . lispy-mode)))
 
 
-;; fedora defaults to older guile 2 version, need to explicitly set the used guile version
-;; ~sudo dnf install guile30~
+;; fedora defaults to older guile 2 version, need to explicitly set the used
+;; guile version ~sudo dnf install guile30~
+
 (use-package geiser
   :config
   (setq geiser-guile-binary "guile3.0")
@@ -557,9 +734,20 @@
 (use-package geiser-guile
   :after geiser)
 
+;;;; Markdown
+
 (use-package markdown-mode
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown"))
+
+;;;; LSP + Parser
+
+;;;;; treesitter
+
+;; use treesitter to handle more complex syntax trees for better highlighting.
+;; Included since emacs 29+
+;;
+;; treesit-auto to install treesitter major modes automatically
 
 (use-package treesit-auto
   :custom
@@ -568,10 +756,16 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
+;;;;; eglot
+
+;; try something lighter with eglot
+
 (use-package eglot
   :hook ((typescript-ts-mode . eglot-ensure)
          (tsx-ts-mode . eglot-ensure)
          (js-ts-mode . eglot-ensure)))
+
+;;; RSS
 
 (use-package elfeed)
 
@@ -580,3 +774,7 @@
   :config
   (elfeed-org)
   (setq rmh-elfeed-org-files (list "~/Sync/org/bookmarks.org")))
+
+;;; Misc
+
+;;; init.el ends here
